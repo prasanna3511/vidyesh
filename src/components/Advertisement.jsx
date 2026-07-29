42d893aa2febe5cdea8554c073ba17119f9cac40
Prasanna Joshi <prasannaj@ithena.ai>
2026-07-29 08:15:36 +0530

    advertisement

--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -4,21 +4,23 @@ import advertise from '../assets/1000373663.png'
 export default function Advertisement({ onClose }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative  rounded-lg shadow-lg w-full max-w-md h-[90vh] mt-[5vh] mb-[5vh] overflow-hidden">
-        {/* Close Button */}
-        <button
-          onClick={onClose}
-          className="absolute right-8 text-gray-500 hover:text-red-600 text-xl font-bold z-10"
-        >
-          &times;
-        </button>
+      <div className="w-full max-w-md h-[90vh] mt-[5vh] mb-[5vh] flex items-center justify-center">
+        <div className="relative inline-block">
+          {/* Close Button */}
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-2 text-gray-500 hover:text-red-600 text-xl font-bold z-10"
+          >
+            &times;
+          </button>
 
-        {/* Image */}
-        <img
-          src={advertise}
-          alt="Ad"
-          className="h-full w-full object-contain rounded"
-        />
+          {/* Image */}
+          <img
+            src={advertise}
+            alt="Ad"
+            className="max-h-[90vh] w-full object-contain rounded shadow-lg"
+          />
+        </div>
       </div>
     </div>
   );
